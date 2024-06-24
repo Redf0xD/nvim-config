bac5fe2c71950d67c9d9dea1781b12e8d26f8853
ricardoprato <ricardoprato36@gmail.com>
2024-06-23 22:14:03 -0300

    🎨 Replace colon with <cmd>

--- lua/redfoxd/alpha.lua
+++ lua/redfoxd/alpha.lua
@@ -15,13 +15,13 @@ function M.config()
     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
   }
   dashboard.section.buttons.val = {
-    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-    dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    dashboard.button("f", " " .. " Find file", "<cmd>Telescope find_files <CR>"),
+    dashboard.button("e", " " .. " New file", "<cmd>ene <BAR> startinsert <CR>"),
+    dashboard.button("p", " " .. " Find project", "<cmd>lua require('telescope').extensions.projects.projects()<CR>"),
+    dashboard.button("r", "󰄉 " .. " Recent files", "<cmd>Telescope oldfiles <CR>"),
+    dashboard.button("t", " " .. " Find text", "<cmd>Telescope live_grep <CR>"),
+    dashboard.button("c", " " .. " Config", "<cmd>e $MYVIMRC <CR>"),
+    dashboard.button("q", " " .. " Quit", "<cmd>qa<CR>"),
   }
   local function footer()
     return "ricardoprato.com"
